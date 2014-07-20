4aeddbfe150d00188bc3f2f4168845d07649c996
Daphne Chong <daphne.h.chong@gmail.com>
2014-07-20 23:23:02 +1000

    move all calculator methods into calculator object

--- week1.js
+++ week1.js
@@ -1,5 +1,9 @@
 (function() {
 	function calculator() {
+		var currentNumber = ''
+		var numbers = [];
+		var operators = [];
+
 		function add(a, b) {
 			return a + b;
 		}
@@ -16,50 +20,54 @@
 			return a / b;
 		}
 
+		function appendNumber(n) {
+			currentNumber += n;
+			console.log(currentNumber);
+		}
+
+		function appendOperator(o) {
+			console.log(o);
+			operators.push(o);
+			numbers.push(parseInt(currentNumber));
+			currentNumber = '';
+		}
+
+		function clearCalculations() {
+			numbers = [];
+			currentNumber = '';
+			operators = [];
+		}
+
+		function equals() {
+			numbers.push(parseInt(currentNumber));
+
+			var total = numbers[0];
+			for(var i = 1; i < numbers.length; i++) {
+				var operator = operators.shift();
+
+				// not sure how to just execute operator(total, numbers[i]) without having to qualify "this"? I get a "string is not a function" error
+				total = this[operator](total, numbers[i]); 
+			}
+
+			clearCalculations();
+			return total;
+		}
+
 		return {
+			appendNumber: appendNumber,
+			appendOperator: appendOperator,
+			clear: clearCalculations,
+			equals: equals,
+			currentValue: currentNumber,
 			add: add,
 			subtract: subtract,
-			multiply: multiply,
-			divide: divide
+			divide: divide,
+			multiply: multiply
 		};
 	}
 
 	var calculator = calculator();
-	var currentNumber = ''
-	var numbers = [];
-	var operators = [];
-	
-	function appendNumber(n) {
-		currentNumber += n;
-		console.log(currentNumber);
-	}
 
-	function appendOperator(o) {
-		console.log(o);
-		operators.push(o);
-		numbers.push(parseInt(currentNumber));
-		currentNumber = '';
-	}
-
-	function clearCalculations() {
-		numbers = [];
-		currentNumber = '';
-		operators = [];
-		updateResultWindow('');
-	}
-
-	function equals() {
-		numbers.push(parseInt(currentNumber));
-
-		var total = numbers[0];
-		for(var i = 1; i < numbers.length; i++) {
-			var operator = operators.shift();
-			total = calculator[operator](total, numbers[i]);
-		}
-
-		clearCalculations();
-		return total;
-	}
 	
 	function updateResultWindow(result) {
 		document.getElementById("result").innerHTML = result;
@@ -72,18 +80,19 @@
 		}
 	}
 
-	subscribeClickEvents("operator", appendOperator);
+	subscribeClickEvents("operator", calculator.appendOperator);
 	subscribeClickEvents("number", function(id) { 
-		appendNumber(id); 
-		updateResultWindow(currentNumber);
+		calculator.appendNumber(id); 
+		updateResultWindow(calculator.currentValue);
 	});
 
 	document.getElementById("equals").addEventListener('click', function() { 
-		updateResultWindow(equals());
+		updateResultWindow(calculator.equals());
 	});
 
 	document.getElementById("clear").addEventListener('click', function() { 
-		clearCalculations();
+		calculator.clear();
+		updateResultWindow('');
 	});
 
 }());
