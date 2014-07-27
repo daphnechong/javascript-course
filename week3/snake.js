bc97c31dab9767856e3744d1898d443eff6659de
Daphne Chong <daphne.h.chong@gmail.com>
2014-07-28 00:13:54 +1000

    add some tests & split game class out into its own js file

--- week3/snake.js
+++ week3/snake.js
@@ -50,66 +50,6 @@ function snake() {
 	}
 }
 
-function game() {
-	var grid = [];
-	var gridSize = 40;
-	
-	function setupGrid() {
-		for (var i = 0; i < gridSize; i++) {
-			grid[i] = [];
-			for (var j = 0; j < gridSize; j++) {
-				grid[i][j] = ' ';
-			}
-		}
-	}
-
-	function render() { 
-		for (var i = 0; i < grid.length; i++) {
-			var row = $('<div>').addClass('row');
-			$('#container').append(row);
-			for (var j = 0; j < grid[i].length; j++) {
-				var cell = $('<div>').addClass('cell').text(grid[j][i]);
-				row.append(cell);
-			}
-		}
-	}
-
-	function setup(initialSnake, symbol) {
-		setupGrid();
-		
-		for (var i = 0; i < initialSnake.length; i++) {
-			updateGridSquare(initialSnake[i], symbol);
-		}
-
-		render();
-	}
-
-	function isArrowKey(e) {
-		return e.keyCode >= 37 && e.keyCode <= 40
-	}
-
-	function subscribe(handler) {
-		$( 'body' ).keydown(function(e) {
-			if (isArrowKey(e)) {
-				handler(e.originalEvent.keyIdentifier);	  	
-		  	}
-		});
-	}	
-
-	function updateGridSquare(point, value) {
-		grid[point.x][point.y] = value;
-		var row = $('.row').eq(point.y);
-		var cell = row.find('.cell').eq(point.x);
-		cell.text(value);
-	}
-
-	return {
-		subscribeToArrowKeys : subscribe,
-		updateGridSquare: updateGridSquare,
-		setup : setup
-	}
-}
-
 function handleArrowKey(newDirection) {
 	s.changeDirection(newDirection);
 	move();
---
+++ week3/specs/game.spec.js
@@ -0,0 +1,66 @@
+describe('Game', function() {
+	it('should listen to left arrow key presses', function() {
+		var mockHandler = jasmine.createSpy('handler');
+	    var g = game();	
+		g.subscribeToArrowKeys(mockHandler)
+
+		var e = $.Event('keydown');
+		e.keyCode = 37;
+		e.originalEvent = { keyIdentifier: "identifier" };
+		$('body').trigger(e);
+
+		expect(mockHandler).toHaveBeenCalledWith("identifier");
+	});
+
+	it('should listen to right arrow key presses', function() {
+		var mockHandler = jasmine.createSpy('handler');
+	    var g = game();	
+		g.subscribeToArrowKeys(mockHandler)
+
+		var e = $.Event('keydown');
+		e.keyCode = 39;
+		e.originalEvent = { keyIdentifier: "identifier" };
+		$('body').trigger(e);
+
+		expect(mockHandler).toHaveBeenCalledWith("identifier");
+	});
+
+	it('should listen to up arrow key presses', function() {
+		var mockHandler = jasmine.createSpy('handler');
+	    var g = game();	
+		g.subscribeToArrowKeys(mockHandler)
+
+		var e = $.Event('keydown');
+		e.keyCode = 38;
+		e.originalEvent = { keyIdentifier: "identifier" };
+		$('body').trigger(e);
+
+		expect(mockHandler).toHaveBeenCalledWith("identifier");
+	});
+
+	it('should listen to down arrow key presses', function() {
+		var mockHandler = jasmine.createSpy('handler');
+	    var g = game();	
+		g.subscribeToArrowKeys(mockHandler)
+
+		var e = $.Event('keydown');
+		e.keyCode = 40;
+		e.originalEvent = { keyIdentifier: "identifier" };
+		$('body').trigger(e);
+
+		expect(mockHandler).toHaveBeenCalledWith("identifier");
+	});
+
+	it('should ignore other keys being pressed', function() {
+		var mockHandler = jasmine.createSpy('handler');
+	    var g = game();	
+		g.subscribeToArrowKeys(mockHandler)
+
+		var e = $.Event('keydown');
+		e.keyCode = 86;
+		$('body').trigger(e);
+
+		expect(mockHandler).not.toHaveBeenCalled();
+	});
+
+});
