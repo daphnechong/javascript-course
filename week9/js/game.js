59ebc2546a896e0a4a95fcce5ee30b145b16ae7d
Daphne Chong <daphne.h.chong@gmail.com>
2014-09-01 20:16:13 +1000

    position bunkers and cities

--- week9/js/game.js
+++ week9/js/game.js
@@ -2,17 +2,34 @@
 var canvas = document.getElementById('canvas'); 
 var context = canvas.getContext('2d');
 
+drawRepeatingImage('images/ground.png', 0, 495, 800, 105);
 
-var img = new Image();
-img.src = 'images/ground.png';
-img.onload = function() {
-	// context.drawImage(img, , 0);	
-	context.save()
-	var groundPattern = context.createPattern(img, 'repeat-x');
-	context.fillStyle = groundPattern;
-	context.translate(0, 495);
-	context.fillRect(0, 0, 800, 105);
+drawImage('images/bunker.png', 80, 490);
+drawImage('images/bunker.png', 320, 480);
+drawImage('images/bunker.png', 600, 510);
+drawImage('images/city.png', 200, 490);
+drawImage('images/city.png', 450, 500);
+drawImage('images/city.png', 700, 490);
 
-	context.restore();
+
+function drawImage(src, x, y) {
+	var img = new Image();
+	img.src = src;
+	img.onload = function() {
+		context.drawImage(img, x, y);
+	}
 }
 
+function drawRepeatingImage(src, x, y, width, height) {
+	var img = new Image();
+	img.src = src;
+	img.onload = function() {
+		context.save()
+		var pattern = context.createPattern(img, 'repeat-x');
+		context.fillStyle = pattern;
+		context.translate(x, y);
+		context.fillRect(0, 0, width, height);
+
+		context.restore();
+	}
+}
