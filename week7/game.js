56159b7a37ee0f81db76ec4898869d57b1087356
Daphne Chong <daphne.h.chong@gmail.com>
2014-09-01 18:39:05 +1000

    right click to flag a square

--- week7/game.js
+++ week7/game.js
@@ -1,5 +1,6 @@
 var Game = function(board, bombCount) {
 	var self = this;
+	var flaggedCount = 0;
 	this.bombCount = bombCount;
 	this.board = board;
 
@@ -10,12 +11,32 @@ var Game = function(board, bombCount) {
 	
 	// need to extract the render into a different presentaiton layer.
 
-	$('.cell').click(function(e) {
-		var coords = e.target.id.split('-');
-		var x = parseFloat(coords[0]);
-		var y = parseFloat(coords[1]);
-		self.board.reveal(x, y);
+	var extractCoordinates = function(elementId) {
+		var coords = elementId.split('-');
+			var x = parseFloat(coords[0]);
+			var y = parseFloat(coords[1]);
+			return {x: x, y:y}
+	}
+
+	$('.cell').on('mousedown', function(e) {
+		if (e.which === 1) {
+			var coords = extractCoordinates(e.target.id);
+			self.board.reveal(coords.x, coords.y);
+		}
 	});
+
+	$('.cell').on('contextmenu', function(e) {
+		var coords = extractCoordinates(e.target.id);
+		var cell = self.board.getCell(coords.x, coords.y);
+		cell.isFlagged = !cell.isFlagged;
+		flaggedCount = flaggedCount + (cell.isFlagged ? 1 : -1);
+
+		console.log('flagged', flaggedCount)
+		if (flaggedCount === bombCount) {
+			console.log('you win!');
+		}
+		return false;
+	})
 	// subscribe to click events on the grid cells
 	// on click, 
 	// 		if it's an empty cell, you want to expose all the others in the immediate region
